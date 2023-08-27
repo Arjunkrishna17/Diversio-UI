73ab5d28999c0ee7b06cddf6352cf6f733f82f9b
Krishna-Arjun <107336846+Arjunkrishna17@users.noreply.github.com>
2023-08-27 16:50:08 +0530

    MF-91  responsive search bar

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as SearchIcon } from "../Images/Search.svg";
 
 const Search = () => {
   return (
-    <div className="flex w-full relative justify-center items-center">
+    <div className="flex w-full relative justify-center items-center max-w-2xl">
       <input type="text" className="searchBar " />
       <SearchIcon className="w-5 h-5 absolute right-5 hover:fill-orange-500 cursor-pointer"/>
     </div>
